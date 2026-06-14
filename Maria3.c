d8dda290a5c1bb5abc4cd17efba2343a84f92032
mariaafroj018-ai <mariaafroj018@gmail.com>
2026-06-14 12:38:22 +0600

    Create Maria3.c

---
+++ Maria3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int date = 10;
+    int month = 8;
+    int year = 2005;
+    int result = date * month * year;
+    
+    printf("Multiplication Result: %d", result);
+    return 0;
+}
