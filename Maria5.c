13828f0e7c7eea80629a045724bc5828b24108af
mariaafroj018-ai <mariaafroj018@gmail.com>
2026-06-14 12:41:10 +0600

    Create Maria5.c

---
+++ Maria5.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int num1 = 10;
+    int num2 = 8;
+    int num3 = 2005;
+    int average = (num1 + num2 + num3) / 3;
+    
+    printf("The Average Value: %d", average);
+    return 0;
+}
