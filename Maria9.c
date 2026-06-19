9f36086960e0384f39aed26256fcb678ac12e2c0
mariaafroj018-ai <mariaafroj018@gmail.com>
2026-06-20 01:02:59 +0600

    Create Maria9.c

---
+++ Maria9.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int number = 5;
+    
+    if (number == 5) {
+        printf("Yes, it is 5");
+    } else {
+        printf("No, it is not 5");
+    }
+    
+    return 0;
+}
