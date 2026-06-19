9ed472d34ddbc99aaa6b0be94c7d5d1686442454
mariaafroj018-ai <mariaafroj018@gmail.com>
2026-06-20 01:03:41 +0600

    Create Maria10.c

---
+++ Maria10.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int time = 10;
+    
+    if (time == 10) {
+        printf("Lab class starts now");
+    } else {
+        printf("Wrong time");
+    }
+    
+    return 0;
+}
