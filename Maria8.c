26654d0e83681a35801b74a7508131a6d86fec4d
mariaafroj018-ai <mariaafroj018@gmail.com>
2026-06-20 00:58:39 +0600

    Create Maria8.c

---
+++ Maria8.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    float maria = 10.8;
+    
+    if (maria > 10.5) {
+        printf("Maria is greater than 10.5");
+    } else {
+        printf("Maria is less than 10.5");
+    }
+    
+    return 0;
+}
